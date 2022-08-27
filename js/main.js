9e0e478f4059350cedee4d3670e48802cfc7ad99
AYGJ <jofre1236@gmail.com>
2022-08-27 12:56:04 -0500

    Actualizando la API

--- js/main.js
+++ js/main.js
@@ -2,8 +2,16 @@
 
 
 function getAPI() {
-   
-    
+
+    const access_token = 'frNC86dJ1THPnIaUIBCJQYpbTGYP30DJLtevLBId8X2FuydllyK';
+    const expires = '3600';
+    const code = 'fr3118fc52de734f0777c7653972b696';
+    let url = `https://api.deezer.com/album/302127`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(resultado => console.log(resultado))
+        .catch(err => console.log(err));
 }
 
 
